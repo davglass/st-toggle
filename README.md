609572faecf44634082ca8d4ae3dcdbe2994cbdf
Dav Glass <davglass@gmail.com>
2015-04-06 13:24:55 -0500

    Make it so

---
+++ README.md
@@ -0,0 +1,8 @@
+Toggle Power on Schedule
+========================
+
+Simple SmartApp to toggle the power of a switch every `x` hours.
+
+On each iteration if the switch is `off` it will set it to `on`.
+
+I use this for my Hot Tub, every 4 hours it will toggle the power to it.
---
+++ toggle.groovy
@@ -0,0 +1,70 @@
+/**
+ *  Toggle Power on Schedule
+ *
+ *  Copyright 2015 Dav Glass
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Toggle Power on Schedule",
+    namespace: "davglass",
+    author: "Dav Glass",
+    description: "Toggle the power of an outlet on a set schedule",
+    category: "Green Living",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("Outlets to toggle:") {
+        input "outlets", "capability.switch", title: "Outlets", required: true, multiple: true
+    }
+    section("Hours to leave on/off:") {
+        input "hours", "number", title: "Hours", required: true
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    def cron = "0 0 0/${hours} * * ?"
+    log.debug "Setting up cron with: ${cron}"
+    schedule(cron, toggleSwitches); 
+}
+
+def toggleSwitches() {
+    outlets.each {
+        def currentValue = it.currentValue("switch");
+        def msg = "${it.displayName} is ${currentValue}, switching it "
+        if (currentValue == "on") {
+            msg += "off"
+            it.off();
+        } else {
+            msg += "on"
+            it.on();
+        }
+        log.debug msg;
+        sendNotificationEvent(msg)
+    }
+}
+
